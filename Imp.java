5e12e90b0dd7c6adb86b73bcdbba94fd3b7f0674
manavlade <manavlade14690@gmail.com>
2024-02-08 20:26:46 +0530

    Added imp question

--- Imp.java
+++ Imp.java
@@ -96,4 +96,28 @@ public class Imp {
         return root;
     }
 
+/*
+ * Question
+ * For a given integer array/list 'ARR' of size 'N' containing all distinct
+ * values, find the total number of 'Inversions' that may exist.
+ * An inversion is defined for a pair of integers in the array/list when the
+ * following two conditions are met.
+ * A pair ('ARR[i]', 'ARR[j]') is said to be an inversion when:
+ * 1. 'ARR[i] > 'ARR[j]'
+ * 2. 'i' < 'j'
+ * Where 'i' and 'j' denote the indices ranging from [0, 'N').
+ */
+    public static long getInversions(long arr[], int n) 
+    {
+        long count = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = arr.length-1; j > i; j--) {
+                if(arr[i] > arr[j]){
+                    count++;
+                }
+            }
+        } 
+        return count;  
+    }
+
 }
