8c5fe4c7a3c2f27b1ce626317531970d0e6ad184
manavlade <manavlade14690@gmail.com>
2024-02-17 04:55:29 +0530

    Bridge function

--- Graph.java
+++ Graph.java
@@ -7,7 +7,7 @@ import java.util.Stack;
 public class Graph {
     static class Edge {
         int start;
-        int destination;   
+        int destination;
 
         public Edge(int s, int d) {
             this.start = s;
@@ -15,7 +15,7 @@ public class Graph {
         }
     }
 
-    public static void CreateGraph( ArrayList<Edge> graph[]){
+    public static void CreateGraph(ArrayList<Edge> graph[]) {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<Edge>();
         }
@@ -41,43 +41,43 @@ public class Graph {
         graph[5].add(new Edge(5, 6));
     }
 
-    public static void bFS(ArrayList<Edge> graph[], int V, boolean vis[], int start){
+    public static void bFS(ArrayList<Edge> graph[], int V, boolean vis[], int start) {
         Queue<Integer> q = new LinkedList<>();
-        
+
         q.add(start);
 
-       while (!q.isEmpty()) {
-        int curr = q.remove();
-        if(vis[curr] = false){
-            System.out.print(curr + " ");
-            vis[curr] = true;
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            if (vis[curr] = false) {
+                System.out.print(curr + " ");
+                vis[curr] = true;
+            }
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                q.add(e.destination);
+            }
         }
-        for (int i = 0; i < graph[curr].size(); i++) {
-            Edge e =graph[curr].get(i);
-            q.add(e.destination);
-        }
-       }
-       
+
     }
 
-    public static void dFS(ArrayList<Edge> graph[], int curr, boolean vis []){
+    public static void dFS(ArrayList<Edge> graph[], int curr, boolean vis[]) {
         vis[curr] = true;
         System.out.print(curr + " ");
         for (int i = 0; i < graph[curr].size(); i++) {
             Edge e = graph[curr].get(i);
-            if(!vis[e.destination]){ // imp if condition in no condition error 
+            if (!vis[e.destination]) { // imp if condition in no condition error
                 dFS(graph, e.destination, vis);
             }
         }
     }
 
-    public static void allPair(ArrayList<Edge> graph[],boolean vis [], int current, String path, int tar){
-        if(current ==  tar){
+    public static void allPair(ArrayList<Edge> graph[], boolean vis[], int current, String path, int tar) {
+        if (current == tar) {
             return;
         }
         for (int i = 0; i < graph[current].size(); i++) {
             Edge e = graph[current].get(i);
-            if(!vis[current]){
+            if (!vis[current]) {
                 vis[current] = true;
                 allPair(graph, vis, e.destination, path + e.destination, tar);
                 vis[current] = false;
@@ -85,25 +85,24 @@ public class Graph {
         }
     }
 
-    public static boolean allCycle(ArrayList<Edge> graph[],boolean vis [], boolean rec [], int current){
+    public static boolean allCycle(ArrayList<Edge> graph[], boolean vis[], boolean rec[], int current) {
         vis[current] = true;
-        rec[current] =  true;
-       for (int i = 0; i < graph[current].size(); i++) {
-           Edge e = graph[current].get(i);
-           if(rec[e.destination]){
-            return true;
-           }
-           else if(!vis[e.destination]){
-            if(allCycle(graph, vis, rec, e.destination)){
+        rec[current] = true;
+        for (int i = 0; i < graph[current].size(); i++) {
+            Edge e = graph[current].get(i);
+            if (rec[e.destination]) {
                 return true;
+            } else if (!vis[e.destination]) {
+                if (allCycle(graph, vis, rec, e.destination)) {
+                    return true;
+                }
             }
-           }
-       }
-       rec[current]= false;
-       return false;
+        }
+        rec[current] = false;
+        return false;
     }
 
-    public static void toplogicalSort(ArrayList<Edge> graph[],boolean vis [], int current, Stack<Integer> stack){
+    public static void toplogicalSort(ArrayList<Edge> graph[], boolean vis[], int current, Stack<Integer> stack) {
         vis[current] = true;
         for (int i = 0; i < graph[current].size(); i++) {
             Edge E = graph[current].get(i);
@@ -114,48 +113,49 @@ public class Graph {
         stack.push(current);
     }
 
-    public static void toputil(ArrayList<Edge> graph[], int V ){
-        boolean visited [] = new boolean[V];
+    public static void toputil(ArrayList<Edge> graph[], int V) {
+        boolean visited[] = new boolean[V];
         Stack<Integer> stack = new Stack<>();
-       for (int i = 0; i < V; i++) {
-        if(!visited[i]){
-            toplogicalSort(graph, visited, i, stack);
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                toplogicalSort(graph, visited, i, stack);
+            }
         }
-       }
 
         while (!stack.empty()) {
             System.out.print(stack.pop() + " ");
         }
     }
-    public static boolean isCycleUndirected(ArrayList<Edge> graph[],boolean vis [], int current, int par){
-        //O(V+E)
-        vis[current]= true;
+
+    public static boolean isCycleUndirected(ArrayList<Edge> graph[], boolean vis[], int current, int par) {
+        // O(V+E)
+        vis[current] = true;
         for (int i = 0; i < graph[current].size(); i++) {
             Edge e = graph[current].get(i);
-            if(vis[e.destination] && e.destination != par ){
+            if (vis[e.destination] && e.destination != par) {
                 return true;
-            }
-            else if(!vis[e.destination]){
-                if(isCycleUndirected(graph, vis, e.destination, current));
+            } else if (!vis[e.destination]) {
+                if (isCycleUndirected(graph, vis, e.destination, current))
+                    ;
                 return true;
             }
         }
         return false;
     }
 
-    public static void kosaRaju(ArrayList<Edge> graph[], int V){
+    public static void kosaRaju(ArrayList<Edge> graph[], int V) {
         Stack<Integer> stack = new Stack<>();
-        boolean [] vis = new boolean[V];
+        boolean[] vis = new boolean[V];
 
         for (int i = 0; i < V; i++) {
-            if(!vis[i]){
+            if (!vis[i]) {
                 toplogicalSort(graph, vis, i, stack);
             }
         }
 
         ArrayList<Edge> transpose[] = new ArrayList[V];
         for (int i = 0; i < graph.length; i++) {
-            vis[i]= false;
+            vis[i] = false;
             transpose[i] = new ArrayList<Edge>();
         }
         for (int i = 0; i < V; i++) {
@@ -167,32 +167,67 @@ public class Graph {
 
         while (!stack.isEmpty()) {
             int cur = stack.pop();
-            if(!vis[cur]){
+            if (!vis[cur]) {
                 dFS(transpose, cur, vis);
                 System.out.println();
             }
         }
     }
-    
-     public static void main(String[] args) {
+
+    public static void dfs(ArrayList<Edge> graph[], int curr, boolean vis[], int dt[], int low[],
+            int time, int par) {
+                vis[curr]= true;
+                dt[curr]= low[curr] = ++time;
+
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    if(e.destination == par){
+                        continue;
+                    }
+                    else if(!vis[e.destination]){
+                        dfs(graph, e.destination, vis, dt, low, time, curr);
+                        low[curr] = Math.min(low[curr], low[e.destination]);
+                        if(dt[curr] < low[e.destination]){
+                            System.out.println("The bridge is " + curr + "--" + e.destination);
+                        }
+                    }
+                    else {
+                        low[curr] = Math.min(low[curr], dt[e.destination]);
+                    }
+                }
+    }
+
+    public static void getBridge(ArrayList<Edge> graph[], int V) {
+        int dt[] = new int[V];
+        int low[] = new int[V];
+        int time = 0;
+        boolean vis[] = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+              dfs(graph, i, vis, dt, low, time, -1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         int V = 7;
-        ArrayList<Edge> graph[]  = new ArrayList[V];
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        // kosaRaju(graph, V);
         CreateGraph(graph);
-        kosaRaju(graph, V);
-        
-        // toputil(graph, V); 
-         
+        getBridge(graph, V);
+
+        // toputil(graph, V);
+
         // Ye part tabhi add karna jab graph tukdo mei bata hoga
         // boolean vis [] = new boolean[V];
         // for (int i = 0; i < V; i++) {
-        //     if(vis[i] == false){
-        //         bFS(graph, V, vis, i);
-        //     }
+        // if(vis[i] == false){
+        // bFS(graph, V, vis, i);
         // }
-  
+        // }
+
         // System.out.println();
-
-
 
         // Printing neughbours of 2
         // for (int i = 0; i < graph.length; i++) {
@@ -205,13 +240,13 @@ public class Graph {
         // boolean recursion [] = new boolean[V];
 
         // for (int i = 0; i < V; i++) {
-        //     if(!visited[i]){
-        //        boolean iscycle =  allCycle(graph, visited, recursion, 0);
-        //        if(iscycle){
-        //         System.out.println(iscycle);
-        //         break;
-        //        }
-        //     }
+        // if(!visited[i]){
+        // boolean iscycle = allCycle(graph, visited, recursion, 0);
+        // if(iscycle){
+        // System.out.println(iscycle);
+        // break;
+        // }
+        // }
         // }
     }
 }
