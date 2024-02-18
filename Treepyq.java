f245f5f2d08730ef60da92bc37286a3f2f93a31b
manavlade <manavlade14690@gmail.com>
2024-02-18 18:28:05 +0530

    Added imp question

--- Treepyq.java
+++ Treepyq.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 class TreeNode {
@@ -108,6 +110,47 @@ public class Treepyq {
         }
 
         return (leftR != null) ? leftR : rightR;
+    }
+
+
+    /*
+     * Given the root of a binary tree, return the average value of the nodes on
+     * each level in the form of an array. Answers within 10-5 of the actual answer
+     * will be accepted.
+     * Example 1:
+     * Input: root = [3,9,20,null,null,15,7]
+     * Output: [3.00000,14.50000,11.00000]
+     * Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5,
+     * and on level 2 is 11.
+     * Hence return [3, 14.5, 11]. 
+     */
+    private List<Long> sum = new ArrayList<>();
+    private List<Integer> count = new ArrayList<>();
+
+      public List<Double> averageOfLevels(TreeNode root) {
+        dfs(root,0);
+        List<Double> ans = new ArrayList<>();
+        for (int i = 0; i < sum.size(); ++i) {
+            ans.add(sum.get(i) * 1.0 / count.get(i));
+        }     
+        return ans;   
+    }
+
+    private void dfs(TreeNode root, int i){
+        if(root == null){
+            return;
+        }
+
+        if(sum.size() == i){
+            sum.add((long) root.val);
+            count.add(1);
+        }
+        else {
+            sum.set(i, sum.get(i) + root.val);
+            count.set(i, count.get(i) + 1);
+        }
+        dfs(root.left, i+1);
+        dfs(root.right, i+1);
     }
 
 }
