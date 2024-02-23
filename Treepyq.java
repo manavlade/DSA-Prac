94e0d68bd9fb4a3985461a318f4c712e3386b67f
manavlade <manavlade14690@gmail.com>
2024-02-23 10:18:53 +0530

    prac

--- Treepyq.java
+++ Treepyq.java
@@ -234,4 +234,35 @@ public class Treepyq {
         }
         return result;
     }
+
+
+    int max = Integer.MIN_VALUE;
+    public int maxPath(TreeNode root) {
+        /*
+         * Question
+         * A path in a binary tree is a sequence of nodes where each pair of adjacent
+         * nodes in the sequence has an edge connecting them. A node can only appear in
+         * the sequence at most once. Note that the path does not need to pass through
+         * the root.
+         * The path sum of a path is the sum of the node's values in the path.
+         * Given the root of a binary tree, return the maximum path sum of any non-empty
+         * path.
+         * Input: root = [-10,9,20,null,null,15,7]
+         * Output: 42
+         * Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7
+         * = 42.
+         */
+        if (root == null)
+            return 0;
+        int value = root.val;
+        int left_sum = Math.max(maxPath(root.left), 0);
+        int right_sum = Math.max(maxPath(root.right), 0);
+        max = Math.max(max, left_sum + right_sum + value);
+        return Math.max(left_sum, right_sum) + value;
+    }
+    public int maxPathSum(TreeNode root) {
+        maxPath(root);
+        return max;
+    }
+
 }
