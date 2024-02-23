ad6471db0fa9526b6dc382a105c103130682defe
manavlade <manavlade14690@gmail.com>
2024-02-23 23:37:25 +0530

    ADDED IMP LINKEDLIST QUESTION

--- LinkedList.java
+++ LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class LinkedList {
     class Node {
         int data;
@@ -49,6 +51,71 @@ public class LinkedList {
             return length;
 
         }
+    }
+
+    public static void removeLoop(Node head) {
+        /*
+        imp question
+         * Question
+         * Given a linked list of N nodes such that it may contain a loop.
+         * 
+         * A loop here means that the last node of the link list is connected to the
+         * node at position X(1-based index). If the link list does not have any loop,
+         * X=0.
+         * Remove the loop from the linked list, if it is present, i.e. unlink the last
+         * node which is forming the loop.
+         */
+        // remove the loop without losing any nodes
+        HashSet<Node> hs = new HashSet<>();
+        if(head.next == null){
+            return;
+        }
+        Node currentNode = head;
+        Node prev = null;
+
+        while (currentNode != null) {
+            if(hs.contains(currentNode)){
+                prev.next = null;
+                break;
+            }
+            else {
+                hs.add(currentNode);
+            }
+            prev = currentNode;
+            currentNode = currentNode.next;
+        }
+    }
+
+    int intersectPoint(Node head1, Node head2) {
+        /*
+        imp question
+         * Question
+         * Internship Alert!
+         * Become an SDE Intern by topping this monthly leaderboard!
+         * banner
+         * Given two singly linked lists of size N and M, write a program to get the
+         * point where two linked lists intersect each other.
+         * Example
+         * LinkList1 = 3->6->9->common
+         * LinkList2 = 10->common
+         * common = 15->30->NULL
+         * Output: 15
+         */
+        if(head1 == null || head2 == null){
+            return -1;
+        }
+
+        Node l1 = head1;
+        Node l2 = head2;
+
+        while (l1 != l2) {
+            l1 = l1 == null ? head2 : l1.next;
+            l2 = l2 == null ? head1 : l2.next;
+        }
+
+        if(l1 == null || l2 == null) return -1;
+
+        return l1.data;
     }
 
 }
