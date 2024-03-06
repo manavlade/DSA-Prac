04ad300f1298e4aa3b285740a1e4ca5ff884d81d
manavlade <manavlade14690@gmail.com>
2024-03-06 07:14:41 +0530

    graph

--- Graph.java
+++ Graph.java
@@ -210,6 +210,49 @@ public class Graph {
         }
     }
 
+
+    public int numIslands(char[][] grid) {
+        /*
+         * Question
+         * Given an m x n 2D binary grid grid which represents a map of '1's (land) and
+         * '0's (water), return the number of islands.
+         * An island is surrounded by water and is formed by connecting adjacent lands
+         * horizontally or vertically. You may assume all four edges of the grid are all
+         * surrounded by water.
+         */
+        if(grid == null || grid.length == 0 || grid[0].length == 0){
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(grid[i][j] == '1'){
+                    count++;
+                    DFS(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    private  void DFS(char[][] grid, int i, int j){
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0'){
+            return;
+        }
+
+        grid[i][j] = '0';
+        DFS(grid, i+1, j);
+        DFS(grid, i-1, j);
+        DFS(grid, i, j+1);
+        DFS(grid, i, j-1);
+
+    }
+
     public static void main(String[] args) {
         int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
