0086141a8381ea0c6ca4ab13e28e555882e4a026
manavlade <manavlade14690@gmail.com>
2024-03-06 23:53:01 +0530

    Graph imp

--- Graph.java
+++ Graph.java
@@ -253,6 +253,64 @@ public class Graph {
 
     }
 
+   
+    public void solve(char[][] board) {
+        /*
+         * Question
+         * Given an m x n matrix board containing 'X' and 'O', capture all regions that
+         * are 4-directionally surrounded by 'X'.
+         * A region is captured by flipping all 'O's into 'X's in that surrounded
+         * region.
+         */
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return;
+        }
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                dfs2(board, i, 0);
+            }
+            if (board[i][n - 1] == 'O') {
+                dfs2(board, i, n - 1);
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                dfs2(board, 0, j);
+            }
+            if (board[m - 1][j] == 'O') {
+                dfs2(board, m - 1, j);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'Y') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs2(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+
+        board[i][j] = 'Y'; // Mark 'O' as a special marker
+
+        // Perform DFS in 4 directions
+        dfs2(board, i + 1, j);
+        dfs2(board, i - 1, j);
+        dfs2(board, i, j + 1);
+        dfs2(board, i, j - 1);
+    }
+
     public static void main(String[] args) {
         int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
