ce23fcde990db9a6f1036f3a4ae7059b63952d2d
manavlade <manavlade14690@gmail.com>
2024-03-26 23:38:21 +0530

    Added imp questions of countWays

--- CodingNinjas.java
+++ CodingNinjas.java
@@ -137,4 +137,40 @@ public class CodingNinjas {
         }
         return sum[n][k];
     }
+
+    //imp Question 
+    public static final int MOD = 1000000007;
+    public static long countDistinctWayToClimbStair(int nStairs) {
+        /*
+         * Question
+         * You have been given a number of stairs. Initially, you are at the 0th stair,
+         * and you need to reach the Nth stair.
+         * Each time, you can climb either one step or two steps.
+         * You are supposed to return the number of distinct ways you can climb from the
+         * 0th step to the Nth step.
+         * Example :
+         * N=3
+         * We can climb one step at a time i.e. {(0, 1) ,(1, 2),(2,3)} or we can climb
+         * the first two-step and then one step i.e. {(0,2),(1, 3)} or we can climb
+         * first one step and then two step i.e. {(0,1), (1,3)}.
+         */
+		if(nStairs == 0 || nStairs == 1 ){
+            return 1;
+        }
+        int n = (int)nStairs;
+        int [] dp =  new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        int count = countWays(n, dp);
+        return count;
+
+	}
+    public static int countWays(long n, int [] dp){
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] % MOD + dp[i-2] %MOD;    
+        }
+        return dp[dp.length-1] % MOD;
+    }
 }
