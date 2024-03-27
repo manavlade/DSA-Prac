e539b524568890a9f22b1e63d1ffea5ec58f1eb6
manavlade <manavlade14690@gmail.com>
2024-03-27 23:56:01 +0530

    Added imp question line  178

--- CodingNinjas.java
+++ CodingNinjas.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class CodingNinjas {
 
@@ -173,4 +174,29 @@ public class CodingNinjas {
         }
         return dp[dp.length-1] % MOD;
     }
+
+     public static List<int[]> pairSum(int[] arr, int s) {
+        /*
+         * Question
+         * You are given an integer array 'ARR' of size 'N' and an integer 'S'. Your
+         * task is to return the list of all pairs of elements such that each sum of
+         * elements of each pair equals 'S'.
+         * Note:
+         * Each pair should be sorted i.e the first value should be less than or equals
+         * to the second value.
+         * Return the list of pairs sorted in non-decreasing order of their first value.
+         * In case if two pairs have the same first value, the pair with a smaller
+         * second value should come first.
+         */
+        List<int[]> pairs = new ArrayList<>();
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] + arr[j] == s){
+                    pairs.add(new int[]{arr[i], arr[j]});
+                }
+            }
+        }
+        return pairs;
+     }
 }
