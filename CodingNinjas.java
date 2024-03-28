7b4cd076180c1a8914a7ceb92a39a870f77fca38
manavlade <manavlade14690@gmail.com>
2024-03-28 23:49:53 +0530

    Upadted code

--- CodingNinjas.java
+++ CodingNinjas.java
@@ -199,4 +199,87 @@ public class CodingNinjas {
         }
         return pairs;
      }
+
+     //Ye vala thoda baaki hai imp hai 
+     static boolean isFeasible(int n, int m, int[] time, long mid) {
+        /*
+         * Question
+         * Ayush is studying for ninjatest which will be held after 'N' days, And to
+         * score good marks he has to study 'M' chapters and the ith chapter requires
+         * TIME[i] seconds to study. The day in Ayush’s world has 100^100 seconds. There
+         * are some rules that are followed by Ayush while studying.
+         * 
+         * 1. He reads the chapter in a sequential order, i.e. he studies i+1th chapter
+         * only after he studies ith chapter.
+         * 
+         * 2. If he starts some chapter on a particular day he completes it that day
+         * itself.
+         * 
+         * 3. He wants to distribute his workload over 'N' days, so he wants to minimize
+         * the maximum amount of time he studies in a day.
+         * 
+         * Your task is to find out the minimal value of the maximum amount of time for
+         * which Ayush studies in a day, in order to complete all the 'M' chapters in no
+         * more than 'N' days.
+         * 
+         * For example
+         * 
+         * if Ayush want to study 6 chapters in 3 days and the time that each chapter
+         * requires is as follows:
+         * Chapter 1 = 30
+         * Chapter 2 = 20
+         * Chapter 3 = 10
+         * Chapter 4 = 40
+         * Chapter 5 = 5
+         * Chapter 6 = 45
+         * 
+         * Then he will study the chapters in the following order
+         */
+         int days = 1;
+         long sum = 0;
+
+         for (int i = 0; i < m; i++) {
+             if (sum + time[i] > mid) {
+                 sum = time[i];
+                 days++;
+             } else {
+                 sum += time[i];
+             }
+         }
+
+         return days <= n;
+     }
+
+     static long maximum(int[] time) {
+         long min = time[0];
+         for (int i = 1; i < time.length; i++) {
+             min = Math.max(time[i], min);
+         }
+         return min;
+     }
+
+     public static long ayushGivesNinjatest(int n, int m, int[] time) {
+         // Write your code here.
+
+         long min = (long) maximum(time);
+         long max = 0L;
+         for (int i = 0; i < m; i++) {
+             max += time[i];
+         }
+
+         long res = Integer.MAX_VALUE;
+
+         while (min <= max) {
+             long mid = (min + max) / 2;
+             if (isFeasible(n, m, time, mid)) {
+                 max = mid - 1;
+                 res = mid;
+             } else {
+                 min = mid + 1;
+             }
+         }
+
+         return res;
+     }
 }
+
