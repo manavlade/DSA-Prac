2787fa195e084f8d3fc93dfca5676fe2720bec28
manavlade <manavlade14690@gmail.com>
2024-06-09 22:48:33 +0530

    PYQ

--- Leetcode.java
+++ Leetcode.java
@@ -155,4 +155,42 @@ public class Leetcode {
         }
         return false;
     }
+
+    // Need to revise the concept of sub arrays also need to practice once
+    public int subarraysDivByK(int[] nums, int k) {
+        /*
+         * Question
+         * Given an integer array nums and an integer k, return the number of non-empty
+         * subarrays that have a sum divisible by k.
+         * 
+         * A subarray is a contiguous part of an array.
+         * Example 1:
+         * Input: nums = [4,5,0,-2,-3,1], k = 5
+         * Output: 7
+         * Explanation: There are 7 subarrays with a sum divisible by k = 5:
+         * [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+         * Example 2:
+         * Input: nums = [5], k = 9
+         * Output: 0
+         */
+        int count = 0, sum = 0, rem;
+        int md[] = new int[k];
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            rem = sum % k;
+            if (rem < 0)
+                rem += k; // Adjust negative remainders to be positive
+            md[rem]++;
+        }
+
+        for (int i = 0; i < k; i++) {
+            if (md[i] > 1)
+                count += (md[i] * (md[i] - 1)) / 2; // Calculate pairs of subarrays with the same remainder
+        }
+
+        count += md[0]; // Add subarrays starting from the beginning which are directly divisible by k
+
+        return count;
+    }
 }
